7ce7b8e0f23cd0eda5038bbb6ba4f7c1ff914b2e
Eva Shek <evashek@hawaii.edu>
2013-11-12 00:41:25 -1000

    fixed bugs

--- app/models/ContactDB.java
+++ app/models/ContactDB.java
@@ -12,48 +12,51 @@ import views.formdata.ContactFormData;
  */
 public class ContactDB {
 
-  private static Map<Long, Contact> contacts = new HashMap<>();
+  private static Map<String, Map<Long, Contact>> contacts = new HashMap<String, Map<Long, Contact>>();
 
   /**
    * Stores the information from the form.
    * @param formData Information from form
    */
-  public static void store(ContactFormData formData) {
-    Contact contact;
-    if (formData.id == 0) {
-      long id = contacts.size() + 1;
-      contact = new Contact(formData.firstName, formData.lastName, formData.telephone, id, formData.telephoneType);
-      contacts.put(id, contact);
+  public static void store(String user, ContactFormData formData) {
+    if (!isUser(user)) {
+      contacts.put(user, new HashMap<Long, Contact>());
     }
-    else {
-      contact = new Contact(formData.firstName, formData.lastName, formData.telephone, formData.id,
-          formData.telephoneType);
-      contacts.put(formData.id, contact);
-    }
+    long id = (formData.id == 0) ? contacts.get(user).size() + 1 : formData.id;
+    Contact contact = new Contact(formData.firstName, formData.lastName, formData.telephone, id,
+        formData.telephoneType);
+    contacts.get(user).put(id, contact);
   }
   
+  private static boolean isUser(String user) {
+    return contacts.containsKey(user);
+  }
+
   /**
    * Retrieves a Contact object based on ID key.
    * @param id ID key
    * @return Associated Contact object
    */
-  public static Contact getContact(long id) {
-    return (Contact) contacts.get(id);
+  public static Contact getContact(String user, long id) {
+    return (Contact) contacts.get(user).get(id);
   }
   
   /**
    * Retrieves list of contacts.
    * @return List
    */
-  public static List<Contact> getContacts() {
-    return new ArrayList<>(contacts.values());
+  public static List<Contact> getContacts(String user) {
+    if (!isUser(user)) {
+      return null;
+    }
+    return new ArrayList<>(contacts.get(user).values());
   }
   
   /**
    * Deletes a contact by specified id.
    * @param id ID key
    */
-  public static void deleteContact(long id) {
-    contacts.remove(id);
+  public static void deleteContact(String user, long id) {
+    contacts.get(user).remove(id);
   }
 }
