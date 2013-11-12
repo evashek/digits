548824db4fbda4d2346f23151fb93a98653c7082
Eva Shek <evashek@hawaii.edu>
2013-11-11 17:59:55 -1000

    Finishing WOD

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -5,9 +5,12 @@ import models.ContactDB;
 import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
+import play.mvc.Security;
 import views.formdata.ContactFormData;
+import views.formdata.LoginFormData;
 import views.formdata.TelephoneTypes;
 import views.html.Index;
+import views.html.Login;
 import views.html.NewContact;
 
 /**
@@ -20,14 +23,16 @@ public class Application extends Controller {
    * @return The resulting home page.  
    */
   public static Result index() {
-    return ok(Index.render(ContactDB.getContacts()));
+    return ok(Index.render("Digits database", Secured.isLoggedIn(ctx()), Secured.getUserInfo(ctx()), 
+        ContactDB.getContacts()));
   }
-  
+
   /**
    * Returns newcontact, a page to add a new contact.
    * @param id ID field
    * @return The NewContact page.
    */
+  @Security.Authenticated(Secured.class)
   public static Result newContact(long id) {
     ContactFormData data;
     if (id == 0) {
@@ -38,37 +43,86 @@ public class Application extends Controller {
     }
     Form<ContactFormData> formData = Form.form(ContactFormData.class).fill(data);
     Map<String, Boolean> telTypes = TelephoneTypes.getTypes(data.telephoneType);
-    return ok(NewContact.render(formData, telTypes));
-    
+    return ok(NewContact.render("Add new contact", Secured.isLoggedIn(ctx()), Secured.getUserInfo(ctx()), 
+        formData, telTypes));
+
   }
-  
+
   /**
    * Returns postcontact, takes the information from the form and adds a new contact.
    * @return The NewContact page.
    */
+  @Security.Authenticated(Secured.class)
   public static Result postContact() {
     Form<ContactFormData> formData = Form.form(ContactFormData.class).bindFromRequest();
     Map<String, Boolean> telTypes;
     if (formData.hasErrors()) {
       System.out.println("Errors exist.");
       telTypes = TelephoneTypes.getTypes();
-      return badRequest(NewContact.render(formData, telTypes));
+      return badRequest(NewContact.render("Add new contact", Secured.isLoggedIn(ctx()), Secured.getUserInfo(ctx()),
+          formData, telTypes));
     }
     else {
       ContactFormData data = formData.get();
       ContactDB.store(data);
       telTypes = TelephoneTypes.getTypes(data.telephoneType);
-      return ok(NewContact.render(formData, telTypes));
+      return ok(NewContact.render("Add new contact", Secured.isLoggedIn(ctx()), Secured.getUserInfo(ctx()), 
+          formData, telTypes));
     }
   }
-  
+
   /**
    * Deletes contact from repository.
    * @param id ID of contact
    * @return the Index page with updated contact table
    */
+  @Security.Authenticated(Secured.class)
   public static Result deleteContact(long id) {
     ContactDB.deleteContact(id);
-    return ok(Index.render(ContactDB.getContacts()));
+    return ok(Index.render("Digits database", Secured.isLoggedIn(ctx()), Secured.getUserInfo(ctx()), ContactDB.getContacts()));
+  }
+
+  /**
+   * Provides the Login page (only to unauthenticated users). 
+   * @return The Login page. 
+   */
+  public static Result login() {
+    Form<LoginFormData> formData = Form.form(LoginFormData.class);
+    return ok(Login.render("Login", Secured.isLoggedIn(ctx()), Secured.getUserInfo(ctx()), formData));
+  }
+
+  /**
+   * Processes a login form submission from an unauthenticated user. 
+   * First we bind the HTTP POST data to an instance of LoginFormData.
+   * The binding process will invoke the LoginFormData.validate() method.
+   * If errors are found, re-render the page, displaying the error data. 
+   * If errors not found, render the page with the good data. 
+   * @return The index page with the results of validation. 
+   */
+  public static Result postLogin() {
+
+    // Get the submitted form data from the request object, and run validation.
+    Form<LoginFormData> formData = Form.form(LoginFormData.class).bindFromRequest();
+
+    if (formData.hasErrors()) {
+      flash("error", "Login credentials not valid.");
+      return badRequest(Login.render("Login", Secured.isLoggedIn(ctx()), Secured.getUserInfo(ctx()), formData));
+    }
+    else {
+      // email/password OK, so now we set the session variable and only go to authenticated pages.
+      session().clear();
+      session("email", formData.get().email);
+      return redirect(routes.Application.index());
+    }
+  }
+
+  /**
+   * Logs out (only for authenticated users) and returns them to the Index page. 
+   * @return A redirect to the Index page. 
+   */
+  @Security.Authenticated(Secured.class)
+  public static Result logout() {
+    session().clear();
+    return redirect(routes.Application.index());
   }
 }
